6d03a18f4d08f0d16bb8abb6e4af336cacf83477
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2026-01-13 19:28:04 -0800

    Deleted verify_column.py

--- add_tip_values_column.sql
+++
@@ -1,5 +0,0 @@
--- Add tip_values column to daily_employee_entries table
--- This column stores dynamic tip requirement values as JSON
-
-ALTER TABLE daily_employee_entries
-ADD COLUMN tip_values TEXT DEFAULT '{}';
